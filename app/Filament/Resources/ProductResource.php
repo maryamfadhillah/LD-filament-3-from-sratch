988ed7909946701f4203309c46af0394326beff6
Maryam F <maryam@student.uns.ac.id>
2024-08-16 16:20:54 +0700

    Select Dropdown with/without belongsTo

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -29,6 +29,14 @@ class ProductResource extends Resource
                 Forms\Components\TextInput::make('price')
                 ->required()
                 ->rule('numeric'),
+                Forms\Components\Select::make('status')
+                ->options([
+                    'in stock' => 'in stock',
+                    'sold out' => 'sold out',
+                    'coming soon' => 'coming soon',
+                ]),
+                Forms\Components\Select::make('category_id') 
+                ->relationship('category', 'name'), 
             ]);
     }
 
@@ -44,7 +52,9 @@ class ProductResource extends Resource
                 ->money('usd') 
                 ->getStateUsing(function (Product $record): float { 
                     return $record->price / 100; 
-                }), 
+                }),
+                Tables\Columns\TextColumn::make('status'), 
+                Tables\Columns\TextColumn::make('category.name')
             ])
             ->filters([
                 //
